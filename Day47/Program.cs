851c5bca81effbc9604ea6f4b96e7ec54e2662ea
Sasikala-1226 <gonuguntasasi@gmail.com>
2025-08-02 12:37:50 +0530

    Keeping One Message

--- Day47/Program.cs
+++ Day47/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification");
+
